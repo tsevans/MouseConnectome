503afc01d1846f0fd0b10c0ce310a9ec7486e390
Tom Evans <tsevans@vt.edu>
2018-04-27 14:30:59 -0400

    Refactored code in main viz file

--- visualize_network.py
+++ visualize_network.py
@@ -20,8 +20,8 @@ def process_file(filename):
     proc_vertices = parse_line_vertices(line_list)
     proc_edges = parse_line_edges(line_list)
     edge_file.close()
-    print('\t# of nodes --> ' + str(len(proc_vertices)))
-    print('\t# of edges --> ' + str(len(proc_edges)))
+    print(str(len(proc_vertices)) + ' vertices')
+    print(str(len(proc_edges)) + ' edges')
     return proc_vertices, proc_edges
 
 
@@ -55,132 +55,94 @@ def parse_line_vertices(line_list):
     return vertex_map.keys()
 
 
-def plot_data(vertices, edges, fname):
+def plot_data(vertices, edges, f_name):
+    """
+    Plot network data in three-dimensional space.
+    :param vertices: List of vertices in the network.
+    :param edges: List of connections in the network.
+    :param f_name: Name of HTML file to generate.
+    """
     graph = ig.Graph(edges, directed=False)
     colors = []
     for v in vertices:
         c = ColorHash(v)
-        # if v.startswith('A'):
-        #     colstr = 'rgb(255,' + str(int(c.rgb[1])) + ',0)'
-        # elif v.startswith('V'):
-        #     colstr = 'rgb(0,' + str(int(c.rgb[1])) + ',255)'
-        # else:
-        #     colstr = 'rgb(0,255,' + str(int(c.rgb[1])) + ')'
-        #colstr = 'rgb(' + str(int(c.rgb[0])) + ',' + str(int(c.rgb[1])) + ',' + str(int(c.rgb[2])) + ')'
-        colstr = colstr = 'rgb(0,255,' + str(int(c.rgb[2])) + ')'
-        colors.append(colstr)
+        colors.append('rgb(%s,%s,%s)' % tuple(c.rgb))
 
     # Spatial layout of graph components
-    layt = graph.layout('kk', dim=3)
+    spatial = graph.layout('kk', dim=3)
 
-    x_vertex = [layt[k][0] for k in range(len(vertices))]  # x-coordinates of nodes
-    y_vertex = [layt[k][1] for k in range(len(vertices))]  # y-coordinates of nodes
-    z_vertex = [layt[k][2] for k in range(len(vertices))]  # z-coordinates of nodes
-    x_edge = []
-    y_edge = []
-    z_edge = []
-    for e in edges:
-        x_edge += [layt[e[0]][0], layt[e[1]][0], None]  # x-coordinates of edge ends
-        y_edge += [layt[e[0]][1], layt[e[1]][1], None]  # y-coordinates of edge ends
-        z_edge += [layt[e[0]][2], layt[e[1]][2], None]  # z-coordinates of edge ends
+    def trace_edges(edge_list, layt):
+        """
+        Build the plot.ly trace to map edges on three-dimensional plane.
+        :param edge_list: List of edges to draw.
+        :param layt: Three-dimensional layout for visualization.
+        :return: Edge trace for plot.ly visualization.
+        """
+        x_edge, y_edge, z_edge = ([] for i in range(3))
+        for e in edge_list:
+            x_edge += [layt[e[0]][0], layt[e[1]][0], None]  # x-coordinates of edge ends
+            y_edge += [layt[e[0]][1], layt[e[1]][1], None]  # y-coordinates of edge ends
+            z_edge += [layt[e[0]][2], layt[e[1]][2], None]  # z-coordinates of edge ends
 
-    edge_trace = Scatter3d(
-        x=x_edge,
-        y=y_edge,
-        z=z_edge,
-        mode='lines',
-        line=Line(color='rgb(0,0,0)', width=1),
-        hoverinfo='none'
-    )
+        edge_trace = Scatter3d(x=x_edge, y=y_edge, z=z_edge,
+                               mode='lines',
+                               line=Line(color='rgb(0,0,0)', width=1),
+                               hoverinfo='none')
+        return edge_trace
 
-    vertex_trace = Scatter3d(
-        x=x_vertex,
-        y=y_vertex,
-        z=z_vertex,
-        mode='markers',
-        name='cortical areas',
-        marker=Marker(
-            symbol='dot',
-            size=9,
-            color=colors,
-            line=Line(color='rgb(50,50,50)', width=0.5)
-        ),
-        text=vertices,
-        hoverinfo='text'
-    )
+    def trace_vertices(vertex_list, layt):
+        """
+        Build the plot.ly trace to map vertices on three-dimensional plane.
+        :param vertex_list: List of vertices to place.
+        :param layt: Three-dimensional layout for visualization.
+        :return: Vertex trace for plot.ly visualization.
+        """
+        vertex_trace = Scatter3d(x=[layt[k][0] for k in range(len(vertex_list))],  # x-coordinates of nodes
+                                 y=[layt[k][1] for k in range(len(vertex_list))],  # y-coordinates of nodes
+                                 z=[layt[k][2] for k in range(len(vertex_list))],  # z-coordinates of nodes
+                                 mode='markers', name='cortical areas',
+                                 marker=Marker(symbol='dot', size=9, color=colors,
+                                               line=Line(color='rgb(50,50,50)', width=0.5)),
+                                 text=vertex_list, hoverinfo='text')
+        return vertex_trace
 
-    axis = dict(showbackground=False,
-                showline=False,
-                zeroline=False,
-                showgrid=False,
-                showticklabels=False,
-                title=''
-                )
+    def build_layout():
+        """
+        Build the plot.ly layout object for the visualization.
+        :return: Plot.ly layout object.
+        """
+        axis = dict(showbackground=False, showline=False, zeroline=False,
+                    showgrid=False, showticklabels=False, title='')
 
-    layout = Layout(
-        title="3D visualization of macaque cortical area network",
-        showlegend=False,
-        scene=Scene(
-            xaxis=XAxis(axis),
-            yaxis=YAxis(axis),
-            zaxis=ZAxis(axis),
-        ),
-        margin=Margin(
-            t=100
-        ),
-        hovermode='closest')
+        layout = Layout(title="3D visualization of macaque cortical area network",
+                        showlegend=False,
+                        scene=Scene(xaxis=XAxis(axis), yaxis=YAxis(axis), zaxis=ZAxis(axis), ),
+                        margin=Margin(t=100),
+                        hovermode='closest')
+        return layout
 
-    data = Data([edge_trace, vertex_trace])
-    fig = Figure(data=data, layout=layout)
-    py.plot(fig, filename='./' + fname + '.html')
+    data = Data([trace_edges(edges, spatial),
+                 trace_vertices(vertices, spatial)])
+    fig = Figure(data=data, layout=build_layout())
+    py.plot(fig, filename='output/' + f_name + '.html')
 
 
-def generate_macaque_one():
-    print('+======================+\n| Macaque Connectome # 1 |\n+======================+')
-    vertices, edges = process_file('macaque_one.txt')
-    plot_data(vertices, edges, 'Macaque_One')
-
-
-def generate_macaque_two():
-    print('+======================+\n| Macaque Connectome # 2 |\n+======================+')
-    vertices, edges = process_file('macaque_two.txt')
-    plot_data(vertices, edges, 'Macaque_Two')
-
-
-def generate_macaque_three():
-    print('+======================+\n| Macaque Connectome # 3 |\n+======================+')
-    vertices, edges = process_file('macaque_three.txt')
-    plot_data(vertices, edges, 'Macaque_Three')
-
-
-def generate_cat():
-    print('+==============+\n| Cat Connectome |\n+==============+')
-    vertices, edges = process_file('cat.txt')
-    plot_data(vertices, edges, 'Cat')
-
-
-def generate_mouse():
-    print('+==================+\n| Mouse Connectome |\n+==================+')
-
-    # g = read_graph_from_file('mouse.txt')
-    # g = lpa(g, 6)
-    # print_graph_info(g)
-    #
-    # node_color = []
-    # for v in g:
-    #     random.seed(g.node[v]['current_label'].keys()[0])
-    #     node_color.append(float(random.random()))
-    # labels = dict([(node, node) for node in g.nodes()])
-    # nx.draw_networkx(g, node_color=node_color, with_labels=False, node_size=25)
-    # plt.show()
-
-    vertices, edges = process_file('mouse.txt')
-    plot_data(vertices, edges, 'Mouse')
+def print_header(filename):
+    """
+    Formatted printing of file name in header of output.
+    :param filename: Name of file to visualize.
+    """
+    raw = filename.split('/')
+    filler = ''
+    for x in range(len(raw[1])):
+        filler += '-'
+    border = '+-----------------------------%s-+' % filler
+    text = '| Processing data for file -> %s |' % raw[1]
+    print(border + '\n' + text + '\n' + border)
 
 
 if __name__ == '__main__':
-    #generate_mouse()
-    generate_cat()
-    # generate_macaque_one()
-    # generate_macaque_two()
-    # generate_macaque_three()
+    test_cat = 'data/cat.txt'
+    print_header(test_cat)
+    vertices, edges = process_file(test_cat)
+    plot_data(vertices, edges, 'Cat')
